1ee9cd451ba3456068c3386a0ea78d21e790348f
walterdyna <alexdyna@gmail.com>
2023-07-16 17:28:34 -0300

    Valor minimo e Maximo

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const cursorText = document.querySelector(".cursor-text");
-    const titleText = document.querySelector("title").textContent;
-  
-    document.addEventListener("mousemove", function(event) {
-      const mouseX = event.clientX;
-      const mouseY = event.clientY;
-  
-      cursorText.style.transform = `translate(${mouseX}px, ${mouseY}px)`;
-    });
-  
-    cursorText.textContent = titleText;
+document.addEventListener("DOMContentLoaded", function () {
+  const cursorText = document.querySelector(".cursor-text");
+  const titleText = document.querySelector("title").textContent;
+
+  document.addEventListener("mousemove", function (event) {
+    const mouseX = event.clientX;
+    const mouseY = event.clientY;
+
+    cursorText.style.transform = `translate(${mouseX}px, ${mouseY}px)`;
   });
 
-function generateNumber(){
+  cursorText.textContent = titleText;
+});
 
-   const min = Math.ceil(document.querySelector('.input-min').value)
-   const max = Math.floor(document.querySelector('.input-max').value)
 
-   const result = Math.floor(Math.random() * (max - min +1)) + min
 
+function generateNumber() {
+
+  const min = Math.ceil(document.querySelector('.input-min').value)
+  const max = Math.floor(document.querySelector('.input-max').value)
+
+  if (min >= max) {
+    alert("O valor minimo tem que ser MENOR que o valor Máximo")
+  } else {
+
+    const result = Math.floor(Math.random() * (max - min + 1)) + min
     alert(result)
 
+  }
 }
 
 
